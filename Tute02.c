57a982ec49f2a646294a4a17b21dc444cdaed9ac
IT21471162 <99194173+IT21471162@users.noreply.github.com>
2022-02-13 11:15:18 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -1,8 +1,14 @@
-/*Exercise 2 - Selection
-
-Write a program to calculate the amount to be paid for a rented vehicle.
-
-•	Input the distance the van has travelled
+//Exercise 2 Write a program to calculate the amount to be paid for a rented vehicle
+#include <stdio.h>
+int main() 
+{
+  int distance;
+  printf(" inp
+  
+  
+  
+  
+•	
 •	The first 30 km is at a rate of 50/= per km.
 •	The remaining distance is calculated at the rate of 40/= per km.
 
@@ -16,9 +22,9 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
 
-int main() {
+
+
   
   return 0;
 }
